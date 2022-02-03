4445a42ff07539441f89c06906039e7524d0b60c
dorumamu <272518@naver.com>
2022-02-03 12:45:24 +0900

    login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 //import React, {Component} from 'react';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
+// pages line
 import Index from './pages/Index';
 import Login from './pages/Login';
-import Nav from './components/Nav';
+import MainPage from './pages/Main';
+import JoinUs from './pages/joinus';
+
 import "swiper/swiper.scss";
 import "swiper/components/navigation/navigation.scss";
 import "swiper/components/pagination/pagination.scss";
+// css line
 import "./styles/main.css"
-import MainPage from './pages/Main';
+import "./styles/login.css"
+// components
+import Nav from './components/Nav';
 import Sidebar from './components/Sidebar';
 
 function App() {
@@ -21,6 +27,7 @@ function App() {
           <Route path="/login" element={<Login />}/>
           <Route path="/*" element={<Index />}/>
           <Route path="/main" element={<MainPage/>}/>
+          <Route path="/joinus" element={<JoinUs/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,66 +1,83 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-class Login extends Component{
+class Login extends Component {
 
   constructor(props) {
-      super(props);
-      this.state = {
-          id: "",
-          password: ""
-      };
+    super(props);
+    this.state = {
+      id: "",
+      password: ""
+    };
   };
   handleChange = (e) => {
-      this.setState({
-          [e.target.name]: e.target.value
-      });
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   };
   handleOnClick = (e) => {
-      console.log('click login')
-      console.log('ID :', this.state.id)
-      console.log('PW :', this.state.password)
-      axios.post(this.state.id, this.state.password);
-      this.login()
+    console.log('click login')
+    console.log('ID :', this.state.id)
+    console.log('PW :', this.state.password)
+    axios.post(this.state.id, this.state.password);
+    this.login()
   }
 
   login = () => {
-    fetch('/api/login',{method: 'POST',
-    headers : {
-      'X-XSRF-TOKEN': this.state.csrfToken,
-      'content-type':'application/json',
-    },
-    body: JSON.stringify({
-            id: this.state.id,
-            password: this.state.password
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'X-XSRF-TOKEN': this.state.csrfToken,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: this.state.id,
+        password: this.state.password
       })
     })
-    /*.then(response => response.text())
-    .then(message => {
-      this.setState({message: message});
-      console.log("response : "+this.state.message);
-    });*/
-    .then(response=>{
-      console.log(response)
-      console.log('response.data.userId')
-      console.log('response.data.msg ::',response.formData.msg)
-      if(response.data.userId === undefined){
-        console.log('==========',response.data.msg)
-        alert('입력하신 id 가 일치하지 않습니다.')
-      }
-    })
+      /*.then(response => response.text())
+      .then(message => {
+        this.setState({message: message});
+        console.log("response : "+this.state.message);
+      });*/
+      .then(response => {
+        console.log(response)
+        console.log('response.data.userId')
+        console.log('response.data.msg ::', response.formData.msg)
+        if (response.data.userId === undefined) {
+          console.log('==========', response.data.msg)
+          alert('입력하신 id 가 일치하지 않습니다.')
+        }
+      })
   }
 
-  render(){
-    
+  render() {
+
     return (
-   
+
       <div className="App">
 
         <form className="LoginForm">
-          <h2 className="LoginHeader"> Login페이지 </h2>
-          <input type="text" id="inputId" className="form-control" placeholder="ID" name="id" onChange={this.handleChange}/>
-          <input type="password" id="inputPassword" className="form-control" placeholder="PASSWORD" name="password" onChange={this.handleChange}/>
-          <button className="btn btn-lg btn-primary btn-block" type="button" onClick={this.handleOnClick}> 로그인 </button>
+          <h2 className="LoginHeader">Login</h2>
+          <div className="id_pw_input">
+            <div>
+
+            </div>
+            <div>
+            <input type="text" id="inputId" className="form-control" placeholder="ID" name="id" onChange={this.handleChange} />
+            </div>
+            <div>
+            <input type="password" id="inputPassword" className="form-control" placeholder="PASSWORD" name="password" onChange={this.handleChange} />
+            </div>
+            <div>
+            <button className="btn btn-lg btn-primary btn-block" type="button" onClick={this.handleOnClick}> 로그인 </button>
+            </div>
+          </div>
+          <div>
+            <a className="link_info" href="">아이디 찾기</a>
+            <a className="link_info" href="">비밀번호 찾기</a>
+            <a className="link_info" href="/joinus">회원가입</a>
+          </div>
         </form>
       </div>
     );
